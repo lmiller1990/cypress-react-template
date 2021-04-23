406b63bdd43d65149a386f1dcbd1b998b7e9e0fb
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-04-23 12:36:42 +1000

    work on dashboard

--- src/CustomCommand.spec.tsx
+++ src/CustomCommand.spec.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mount } from '@cypress/react';
 
-it('uses a custom command written in TypeScript', () => {
+xit('uses a custom command written in TypeScript', () => {
   const Comp: React.FC = () => {
     const onClick = cy.stub()
     return (
