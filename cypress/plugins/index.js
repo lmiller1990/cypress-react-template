18355f7093250eee85e5f021785baffb62643b62
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-04-28 11:39:17 +1000

    add lodash

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,11 +1,14 @@
 const findReactScriptsWebpackConfig = require("@cypress/react/plugins/react-scripts/findReactScriptsWebpackConfig")
 const { startDevServer } = require('@cypress/webpack-dev-server')
+const _ = require('lodash')
 
 module.exports = (on, config) => {
+  const map = _.map([4, 8], n => n * 2)
+  console.log(map)
   require('@cypress/code-coverage/task')(on, config)
   const webpackConfig = findReactScriptsWebpackConfig(config)
 
-   const rules = webpackConfig.module.rules.find(rule => !!rule.oneOf).oneOf;
+  const rules = webpackConfig.module.rules.find(rule => !!rule.oneOf).oneOf;
   const babelRule = rules.find(rule => /babel-loader/.test(rule.loader))
   babelRule.options.plugins.push(require.resolve('babel-plugin-istanbul'))
 
