df8990fdcfb165baafc013a0a183309de54cc925
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-05-06 01:14:40 +1000

    update custom command

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -2,6 +2,6 @@
 import '@cypress/code-coverage/support'
 
 // Use TS to make sure support files written in TypeScript work correctly.
-// Cypress.Commands.add('clickButtonWithText', (text: string) => {
-//   return cy.get('button').contains(text).click();
-// });
+Cypress.Commands.add('clickButtonWithText', (text: string) => {
+  return cy.get('button').contains(text).click();
+});
