6ebbe935f2b11b8aed1c1c96413c475ee16a68a8
abhinaba-ghosh <askabhinaba@gmail.com>
2021-06-04 00:51:33 +0530

    added cypress-react-selector example

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 import '@cypress/code-coverage/support'
+import 'cypress-react-selector';
 
 // Use TS to make sure support files written in TypeScript work correctly.
 Cypress.Commands.add('clickButtonWithText', (text: string) => {
--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -6,3 +6,13 @@ it('renders learn react link', () => {
   mount(<App />);
   cy.get('a').contains('Learn React');
 });
+
+
+it('renders app and fetch elemnet using react-selector', () => {
+  mount(<App />);
+
+// cypress-react-selector helps identifying elements using native react pro[erties - component, props and states
+// find more here - https://github.com/abhinaba-ghosh/cypress-react-selector
+  cy.waitForReact();
+  cy.react('App').should('be.visible');
+});
